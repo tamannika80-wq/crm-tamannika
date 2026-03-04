98d1dd3e273e249bc857db326aef6a11327b9a51
tamannika80-wq <tamannika80@gmail.com>
2026-03-04 15:33:08 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,33 @@
-console.log("CRM Tamannika Ready");
+const API_URL = "https://script.google.com/macros/s/AKfycbxTmX6ZONqCPowAz-agZm3_kSmI5F7pIVpLudt1jzX4CegyUNunxQVNl3miwrhTKRZKXg/exec";
+
+function login() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    fetch(API_URL, {
+        method: "POST",
+        body: JSON.stringify({
+            action: "login",
+            username: username,
+            password: password
+        })
+    })
+    .then(res => res.json())
+    .then(data => {
+        if (data.status === "success") {
+
+            // Simpan session login
+            localStorage.setItem("isLoggedIn", "true");
+
+            // Redirect ke dashboard
+            window.location.href = "dashboard.html";
+
+        } else {
+            document.getElementById("message").innerText = "Login gagal!";
+        }
+    })
+    .catch(err => {
+        document.getElementById("message").innerText = "Terjadi kesalahan koneksi.";
+        console.error(err);
+    });
+}
